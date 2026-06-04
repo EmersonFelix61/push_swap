00372c815eecc7425bb54277a510ea1442c7595e
Emerson Da Silva Felix <emda-sil@c2r1s8.42porto.com>
2026-06-04 17:47:32 +0100

    finalizei os parsers (eu acho)

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -4,7 +4,7 @@
 # include "../libft/libft.h"
 # include <limits.h>
 
-typedef struct	s_stack
+typedef struct s_stack
 {
 	int				value;
 	struct s_stack	*next;
@@ -18,10 +18,10 @@ typedef enum e_strategy
 	STRAT_ADAPTIVE
 }	t_strategy;
 
-typedef struct  s_bench
+typedef struct s_bench
 {
 	int			display;
-	double		disorder; 
+	double		disorder;
 	t_strategy	*strategy;
 	int			moves[11];
 	char		*big_o;
@@ -30,7 +30,7 @@ typedef struct  s_bench
 typedef struct s_config
 {
 	t_strategy	strategy;
-	t_bench		*bench; //alterado
+	t_bench		*bench;
 	int			numbers_count;
 }	t_config;
 
@@ -42,16 +42,17 @@ char	*join_args(int argc, char **argv);
 char	**get_tokens(int argc, char **argv);
 void	free_tokens(char **tokens);
 int		print_error(void);
-void    init_config(t_config *config);
-int     set_bench(char *token, t_config *config, t_bench *bench);
-int     set_strategy(char *token, t_config *config, int *strategy_found);
+void	init_config(t_config *config, t_bench *bench);
+int		set_bench(char *token, t_config *config);
+int		set_strategy(char *token, t_config *config, int *strategy_found);
+int		parser_flags(char **tokens, t_config *config);
 
 t_stack	*new_stack(int value);
 void	add_back(t_stack **stack, t_stack *new);
 t_stack	*stack_last(t_stack *stack);
 t_stack	*stack_before_last(t_stack *stack);
 void	stack_free(t_stack **stack);
-t_stack *stack_builder(char **tokens);
+t_stack	*stack_builder(char **tokens);
 
 void	init_bench(t_bench	*bench);
 
--- libft/ft_format.c
+++ libft/ft_format.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "ft_printf.h"
+#include "libft.h"
 
 int	ft_format(char format, va_list args)
 {
